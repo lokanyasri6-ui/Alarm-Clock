3b3ae9662ab9dfc091531fc7be6f334a035483c7
lokanyasri6-ui <lokanyasri6@gmail.com>
2026-06-05 21:08:41 +0530

    Created basic alarm clock

--- alarmclock.py
+++ alarmclock.py
@@ -1,15 +1,14 @@
-from tkinter import*
-from tkinter import messagebox
-import datetime
-def chech_alarm():
-  current_time=datetime.date_time.now().strftime("%H:%M")
-  if current_time==alarm_time.get():messageobx.showinfo("Alarm","Wake Up!")
-  root.after(1000, check_alarm)
-root=Tk()
-root.title("Alarm Clock")
-root.geometry("300x200")
-Label(root, text="Set Alarm (HH:MM)").pack(pady=10)
-alarm_time = StringVar()
-Entry(root,textvariable=alarm_time).pack()
-Button(root,text="Start Alarm",command=check_alarm.pack(pady=10)
-root.mainloop()
+import time
+from datetime import datetime
+
+alarm_time = input("Enter alarm time (HH:MM:SS): ")
+
+while True:
+    current_time = datetime.now().strftime("%H:%M:%S")
+    print(current_time, end="\r")
+
+    if current_time == alarm_time:
+        print("\n⏰ Wake up! Alarm Time Reached!")
+        break
+
+    time.sleep(1)
