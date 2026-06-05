7b53d783f55933db2f63da26120885b3b61e4da1
lokanyasri6-ui <lokanyasri6@gmail.com>
2026-06-05 21:36:36 +0530

    Add Tkinter alarm clock

--- alarmclock.py
+++ alarmclock.py
@@ -1,14 +1,27 @@
-import time
+from tkinter import *
+from tkinter import messagebox
 from datetime import datetime
 
-alarm_time = input("Enter alarm time (HH:MM:SS): ")
+def check_alarm():
+    current_time = datetime.now().strftime("%H:%M")
 
-while True:
-    current_time = datetime.now().strftime("%H:%M:%S")
-    print(current_time, end="\r")
+    if current_time == alarm_time.get():
+        messagebox.showinfo("Alarm", "Wake Up! Alarm Time Reached!")
+    else:
+        root.after(1000, check_alarm)
 
-    if current_time == alarm_time:
-        print("\n⏰ Wake up! Alarm Time Reached!")
-        break
+def start_alarm():
+    check_alarm()
 
-    time.sleep(1)
+root = Tk()
+root.title("Alarm Clock")
+root.geometry("300x200")
+
+Label(root, text="Set Alarm (HH:MM)").pack(pady=10)
+
+alarm_time = StringVar()
+Entry(root, textvariable=alarm_time).pack(pady=5)
+
+Button(root, text="Start Alarm", command=start_alarm).pack(pady=10)
+
+root.mainloop()
